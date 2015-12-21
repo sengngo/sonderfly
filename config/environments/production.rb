91912dd81873e6b4167751f2109f8044b3319ca6
Seng Ngo <sengngo@gmail.com>
2015-12-21 20:55:47 +0000

    added post model, devise, and user)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Sonderfly::Application.configure do
+  config.action_mailer.default_url_options = { host: 'sonderfly.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
---
+++ db/migrate/20151221204823_create_posts.rb
@@ -0,0 +1,23 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+    	t.integer :user_id, null: false
+      t.integer :category_id
+
+      t.string :title
+      t.string :intro
+      t.text :body
+
+      t.string :cover_photo
+      t.string :post_photo
+
+      t.boolean :published, default: false
+      t.boolean :private, default: false
+
+      t.timestamps
+    end
+
+    add_index :posts, :user_id
+    add_index :posts, :category_id
+  end
+end
