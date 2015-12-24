4964a6e619fcda610befe84a161ba7915f808d9e
Seng Ngo <sengngo@gmail.com>
2015-12-24 12:27:57 +0000

    ckeditor

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'fog', '~> 1.3.1'
 
 gem 'mini_magick', '~> 4.3.1'
 
+gem 'ckeditor'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Sonderfly::Application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
   devise_for :users, :controllers => { registrations: 'registrations' }
   root "posts#index"
   resources :posts do
